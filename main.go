a4a911b12585af60776420c0c7d693ead11b98d5
Mateusz Mezyk <matimm@gmail.com>
2020-04-20 21:38:26 +0200

    load json to map

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,16 +9,39 @@ import (
 
 func main() {
 	fmt.Println("App is starting...")
-	loadPlot()
+	plotFile := loadPlot()
+	plotMap := convertJSONToMap(plotFile)
+	fmt.Println(plotMap)
 }
 
-func loadPlot() {
+func loadPlot() []byte {
 	plotFile := "plot.json"
 	fileBytes, err := ioutil.ReadFile(plotFile)
 	if err != nil {
 		log.Fatalln("Cannot open file with story plot definition", plotFile)
 	}
-	fmt.Println(string(fileBytes))
+	return fileBytes
+}
+
+func convertJSONToMap(file []byte) map[string]interface{} {
+	plotMap := new(map[string]interface{})
+	err := json.Unmarshal(file, plotMap)
+	if err != nil {
+		log.Fatalln("Cannot unmarshal json file to expected type", err)
+	}
+	return *plotMap
+}
+
+// check how to use it instead of too generic interface{}
+type plot struct {
+	title   string
+	story   []string
+	options []option
+}
+
+type option struct {
+	text string
+	arc  string
 }
 
 // func loadTemplates() *template.Template {
